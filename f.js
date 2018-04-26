3f943f1feae751bd4d489d7488ff72fa242de94b
Arafat Husayn <arafat@tuta.io>
2018-04-26 17:48:47 +0600

    for facebook

---
+++ f.js
@@ -0,0 +1,67 @@
+(function () {
+
+	function hideAds() {
+		var feeds = document.querySelectorAll('[data-testid="fbfeed_story"]')
+		for ( feed of feeds ) {
+				var spans = feed.querySelectorAll('span')
+				for ( anySpan of spans ) {
+						if( anySpan.querySelector('a') ) {
+								if ( anySpan.querySelector('a')
+								&& /s.?p.?o.?n.?s.?o.?r.?e.?d/gi.test(anySpan.innerText) ) {
+										var p = anySpan.closest('[data-testid*="fbfeed_story"]')
+										if(p) {
+											p.remove()
+										}
+								}
+						}
+				}
+		}
+	}
+
+	function hideAdsOnSides() {
+		if (document.querySelector(".ego_section")) {
+			document.querySelector(".ego_section").style.display = "none"
+		}
+		if (document.querySelector("#GroupsRHCSuggestionSection")) {
+			document.querySelector("#GroupsRHCSuggestionSection").style.display = "none"
+		}
+	}
+
+	function deferredHider(timeoutId) {
+		hideAds()
+		clearTimeout(timeoutId)
+	}
+	
+	var MutationObserver = window.MutationObserver || window.webKitMutationObserver || window.MozMutationObserver
+	var container = document.documentElement || document.body
+	
+	var observer = new MutationObserver(function(mutations) {
+		var timeoutId4 = setTimeout(function() {
+			deferredHider(timeoutId4)
+		}, 5)
+	})
+
+	var config = { attributes: false, childList: true, subtree: true }
+	observer.observe(container, config)
+
+	document.addEventListener("load", function(event) {
+		hideAds()
+		hideAdsOnSides()
+		var timeoutId = setTimeout(function() {
+			deferredHider(timeoutId)
+		}, 100)
+	}, { passive: true })
+	
+	document.addEventListener("DOMContentLoaded", function(event) {
+		hideAds()
+		hideAdsOnSides()
+		var timeoutId2 = setTimeout(function() {
+			deferredHider(timeoutId2)
+		}, 100)
+	  }, { passive: true })
+
+  	var timeoutId3 = setTimeout(function() {
+		deferredHider(timeoutId3)
+	}, 1000)
+	
+})()
