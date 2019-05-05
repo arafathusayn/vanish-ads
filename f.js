e90552a6c912cae7a9e428ddb2948f680320b625
Arafat Husayn <arafat@tuta.io>
2019-05-05 18:30:58 +0600

    make hiding fb ads work again

--- f.js
+++ f.js
@@ -1,67 +1,68 @@
-(function () {
+(function() {
+  function hideAds() {
+    var feeds = document.querySelectorAll('[data-testid="fbfeed_story"]');
+    for (feed of feeds) {
+      var links = feed.querySelectorAll("div a");
+      for (link of links) {
+        if (/s.?p.?o.?n.?s.?o.?r.?e.?d/gi.test(link.innerText)) {
+          var p = link.closest('[data-testid*="fbfeed_story"]');
+          p.remove();
+        }
+      }
+    }
+  }
 
-	function hideAds() {
-		var feeds = document.querySelectorAll('[data-testid="fbfeed_story"]')
-		for ( feed of feeds ) {
-				var spans = feed.querySelectorAll('span')
-				for ( anySpan of spans ) {
-						if( anySpan.querySelector('a') ) {
-								if ( anySpan.querySelector('a')
-								&& /s.?p.?o.?n.?s.?o.?r.?e.?d/gi.test(anySpan.innerText) ) {
-										var p = anySpan.closest('[data-testid*="fbfeed_story"]')
-										if(p) {
-											p.remove()
-										}
-								}
-						}
-				}
-		}
-	}
+  function hideAdsOnSides() {
+    if (document.querySelector(".ego_section")) {
+      document.querySelector(".ego_section").style.display = "none";
+    }
+    if (document.querySelector("#GroupsRHCSuggestionSection")) {
+      document.querySelector("#GroupsRHCSuggestionSection").style.display = "none";
+    }
+  }
 
-	function hideAdsOnSides() {
-		if (document.querySelector(".ego_section")) {
-			document.querySelector(".ego_section").style.display = "none"
-		}
-		if (document.querySelector("#GroupsRHCSuggestionSection")) {
-			document.querySelector("#GroupsRHCSuggestionSection").style.display = "none"
-		}
-	}
+  function deferredHider(timeoutId) {
+    hideAds();
+    clearTimeout(timeoutId);
+  }
 
-	function deferredHider(timeoutId) {
-		hideAds()
-		clearTimeout(timeoutId)
-	}
-	
-	var MutationObserver = window.MutationObserver || window.webKitMutationObserver || window.MozMutationObserver
-	var container = document.documentElement || document.body
-	
-	var observer = new MutationObserver(function(mutations) {
-		var timeoutId4 = setTimeout(function() {
-			deferredHider(timeoutId4)
-		}, 5)
-	})
+  var MutationObserver = window.MutationObserver || window.webKitMutationObserver || window.MozMutationObserver;
+  var container = document.documentElement || document.body;
 
-	var config = { attributes: false, childList: true, subtree: true }
-	observer.observe(container, config)
+  var observer = new MutationObserver(function(mutations) {
+    var timeoutId4 = setTimeout(function() {
+      deferredHider(timeoutId4);
+    }, 5);
+  });
 
-	document.addEventListener("load", function(event) {
-		hideAds()
-		hideAdsOnSides()
-		var timeoutId = setTimeout(function() {
-			deferredHider(timeoutId)
-		}, 100)
-	}, { passive: true })
-	
-	document.addEventListener("DOMContentLoaded", function(event) {
-		hideAds()
-		hideAdsOnSides()
-		var timeoutId2 = setTimeout(function() {
-			deferredHider(timeoutId2)
-		}, 100)
-	  }, { passive: true })
+  var config = { attributes: false, childList: true, subtree: true };
+  observer.observe(container, config);
 
-  	var timeoutId3 = setTimeout(function() {
-		deferredHider(timeoutId3)
-	}, 1000)
-	
-})()
+  document.addEventListener(
+    "load",
+    function(event) {
+      hideAds();
+      hideAdsOnSides();
+      var timeoutId = setTimeout(function() {
+        deferredHider(timeoutId);
+      }, 100);
+    },
+    { passive: true }
+  );
+
+  document.addEventListener(
+    "DOMContentLoaded",
+    function(event) {
+      hideAds();
+      hideAdsOnSides();
+      var timeoutId2 = setTimeout(function() {
+        deferredHider(timeoutId2);
+      }, 100);
+    },
+    { passive: true }
+  );
+
+  var timeoutId3 = setTimeout(function() {
+    deferredHider(timeoutId3);
+  }, 1000);
+})();
