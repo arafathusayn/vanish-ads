f6fe18a4f10936e9102585a4a8ee25166ecc422b
Arafat Husayn <hello@arafat.dev>
2020-08-12 18:46:06 +0600

    update facebook codes for new theme

--- f.js
+++ f.js
@@ -1,11 +1,26 @@
-(function() {
+// @ts-check
+
+(() => {
+  let i = 0;
+  let rightRailAdsRemoved = false;
+
+  const intervalId0 = setInterval(() => {
+    hideAdsOnSides();
+    i++;
+
+    if (i > 100 || rightRailAdsRemoved === true) {
+      clearInterval(intervalId0);
+    }
+  }, 50);
+
   function hideAds() {
-    var feeds = document.querySelectorAll('[data-testid="fbfeed_story"]');
-    for (feed of feeds) {
-      var links = feed.querySelectorAll("div a");
-      for (link of links) {
-        if (/s.?p.?o.?n.?s.?o.?r.?e.?d/gi.test(link.innerText)) {
-          var p = link.closest('[data-testid*="fbfeed_story"]');
+    const feeds = document.querySelectorAll('[data-testid="fbfeed_story"]');
+
+    for (const feed of feeds) {
+      let links = feed.querySelectorAll("div a");
+      for (const link of links) {
+        if (/s.?p.?o.?n.?s.?o.?r.?e.?d/gi.test(link.textContent)) {
+          let p = link.closest('[data-testid*="fbfeed_story"]');
           p.remove();
         }
       }
@@ -13,12 +28,22 @@
 
     try {
       Array.from(document.querySelectorAll("div.userContentWrapper"))
-        .filter(x =>
+        .filter((x) =>
           Array.from(x.querySelectorAll("div"))
-            .reduce((acc, curr) => acc + (curr.innerHTML == "Suggested for You" ? curr.innerHTML : ""), "")
-            .includes("Suggested for You")
+            .reduce(
+              (acc, curr) =>
+                acc +
+                (curr.innerHTML == "Suggested for You" ? curr.innerHTML : ""),
+              "",
+            )
+            .includes("Suggested for You"),
         )
-        .forEach(div => div.parentElement && div.parentElement.parentElement && div.parentElement.parentElement.remove());
+        .forEach(
+          (div) =>
+            div.parentElement &&
+            div.parentElement.parentElement &&
+            div.parentElement.parentElement.remove(),
+        );
     } catch (error) {
       console.log(error);
     }
@@ -26,10 +51,55 @@
 
   function hideAdsOnSides() {
     if (document.querySelector(".ego_section")) {
+      // @ts-ignore
       document.querySelector(".ego_section").style.display = "none";
     }
     if (document.querySelector("#GroupsRHCSuggestionSection")) {
-      document.querySelector("#GroupsRHCSuggestionSection").style.display = "none";
+      // @ts-ignore
+      document.querySelector("#GroupsRHCSuggestionSection").style.display =
+        "none";
+    }
+
+    const sideAdSpans =
+      document.querySelector(`[data-pagelet="RightRail"]`) &&
+      document
+        .querySelector(`[data-pagelet="RightRail"]`)
+        .children[0].querySelectorAll("span");
+
+    if (!sideAdSpans) {
+      return;
+    }
+
+    for (const span of sideAdSpans) {
+      if (span.textContent.includes("Sponsored")) {
+        let target = span;
+
+        for (let i = 0; i < 1000; i++) {
+          if (target.parentElement.innerHTML.includes("facebook.com")) {
+            target.parentElement.style.display = "none";
+
+            const container = target.parentElement.parentElement;
+
+            const observer = new MutationObserver(() => {
+              hideAdsOnSides();
+            });
+
+            const config = {
+              attributes: false,
+              childList: true,
+              subtree: true,
+            };
+
+            observer.observe(container, config);
+
+            rightRailAdsRemoved = true;
+
+            break;
+          } else {
+            target = target.parentElement;
+          }
+        }
+      }
     }
   }
 
@@ -38,43 +108,43 @@
     clearTimeout(timeoutId);
   }
 
-  var MutationObserver = window.MutationObserver || window.webKitMutationObserver || window.MozMutationObserver;
-  var container = document.documentElement || document.body;
+  const MutationObserver = window.MutationObserver;
+  const container = document.documentElement;
 
-  var observer = new MutationObserver(function(mutations) {
-    var timeoutId4 = setTimeout(function() {
+  const observer = new MutationObserver(() => {
+    const timeoutId4 = setTimeout(() => {
       deferredHider(timeoutId4);
     }, 5);
   });
 
-  var config = { attributes: false, childList: true, subtree: true };
+  const config = { attributes: false, childList: true, subtree: true };
   observer.observe(container, config);
 
   document.addEventListener(
     "load",
-    function(event) {
+    () => {
       hideAds();
       hideAdsOnSides();
-      var timeoutId = setTimeout(function() {
+      const timeoutId = setTimeout(() => {
         deferredHider(timeoutId);
       }, 100);
     },
-    { passive: true }
+    { passive: true },
   );
 
   document.addEventListener(
     "DOMContentLoaded",
-    function(event) {
+    () => {
       hideAds();
       hideAdsOnSides();
-      var timeoutId2 = setTimeout(function() {
+      const timeoutId2 = setTimeout(() => {
         deferredHider(timeoutId2);
       }, 100);
     },
-    { passive: true }
+    { passive: true },
   );
 
-  var timeoutId3 = setTimeout(function() {
+  const timeoutId3 = setTimeout(() => {
     deferredHider(timeoutId3);
   }, 1000);
 })();
